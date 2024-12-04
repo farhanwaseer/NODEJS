f2581c4957be64dbefc4d921ae12193c16391767
Farhan_waseer <59307895+farhanwaseer@users.noreply.github.com>
2024-12-04 19:10:40 +0000

    //

--- CurrentWork/models/user.js
+++ CurrentWork/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://127.0.0.1:27017/mongopractice';
+// const uri = 'mongodb://127.0.0.1:27017/mongopractice';
+const uri = `mongodb+srv://farhanaliwaseer:BwF3GSsMcCbEcOYe@cluster0.zanow.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.connect(uri)
 .then(() => console.log('Connected to MongoDB using Mongoose..'))
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NODEJS
 learning nodejs projects
+
+Its a Create User ID App without login!
