065bf94329430e611c8b03da8fe0db5556211bae
Farhan_waseer <59307895+farhanwaseer@users.noreply.github.com>
2024-12-21 22:08:32 +0000

    ...

--- current-work/app.js
+++ current-work/app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require("path");
 const userModel = require('./models/user');
 const postModel = require("./models/post");
+const multer = require("multer");
+const crypto = require("crypto");
 
 const PORT = 3000;
 const jwt = require("jsonwebtoken");
@@ -16,10 +18,26 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/my-uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+});
+
+const upload = multer({ storage: storage })
+
 app.get("/",(req,res)=> {
   res.render("index")
 }
 );
+
+app.get("/test" , (req,res) => {
+  res.render("test");
+})
 
 app.get("/signup",(req,res)=> {
   res.render("signup")
