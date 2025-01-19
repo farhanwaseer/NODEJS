b02718574b6413ca1c3881fca3943ce28effcf62
farhanwaseer <farhanaliwaseer@gmail.com>
2025-01-19 14:22:34 +0500

    /

---
+++ scatch-shop/config/mongoose-connect.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const config = require("config");
+
+const dbgr = require("debug")("development:mongoose");
+
+const uri = `${config.get("MONGODB_URI")}/scatch`;
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    dbgr("Connected to MongoDB using Mongoose");
+  })
+  .catch((err) => dbgr("Error connecting to MongoDB:", err));
+
+module.exports = mongoose.connection;
