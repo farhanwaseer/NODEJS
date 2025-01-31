2a71c254a4e0ee72738f56f95eb1d62da23b42ff
farhanwaseer <farhanaliwaseer@gmail.com>
2025-02-01 01:48:05 +0500

    created structure of app and register route endpoint

--- E-commerces/server.js
+++ E-commerces/server.js
@@ -1,14 +1,25 @@
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
-
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js"
 
 // configure env
 dotenv.config();
 
+// DataBase Connection
+connectDB();
+
 // rest object
 const app = express();
 
+// middelwares
+app.use(express.json());
+app.use(morgan("dev"));
+
+// routes
+app.use("/api/v1/auth", authRoutes)
 // rest api
 app.get("/", (req, res) => {
   res.send("<h1>WelCome to App</h1>");
@@ -17,7 +28,7 @@ app.get("/", (req, res) => {
 // PORT
 const PORT = process.env.PORT || 8080;
 
-// run listen
+// Start your Express server
 app.listen(PORT, () => {
   console.log(`  Server in Running on ${PORT}`.cyan.bold);
 });
