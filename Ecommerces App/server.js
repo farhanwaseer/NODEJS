f0aecd8c267399235c9b202d7ac7eeaa4272dc94
farhanwaseer <farhanaliwaseer@gmail.com>
2025-05-15 03:46:32 +0500

    EcommerceApp

--- Ecommerces App/server.js
+++ Ecommerces App/server.js
@@ -1,10 +1,23 @@
 // const express = require("express");
 import express from "express";
 import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+// config dotenv
+dotenv.config();
+
+// database config
+connectDB();
 
 // rest object
 
 const app = express();
+
+// middleware
+app.use(express.json());
+app.use(morgan("dev"));
 
 // rest api
 
@@ -15,9 +28,12 @@ app.get("/", (req, res) => {
 });
 
 // PORT
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // run listen
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`.cyan.bold);
+  console.log(
+    `Server is running on ${process.env.DEV_MODE} mode on port ${PORT}`.cyan
+      .bold
+  );
 });
