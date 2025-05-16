44e802444157438404e8d56f245cb2ec23b80caa
farhanwaseer <farhanaliwaseer@gmail.com>
2025-05-16 22:27:10 +0500

    Created Pages

--- Ecommerces App/client/src/App.jsx
+++ Ecommerces App/client/src/App.jsx
@@ -1,12 +1,20 @@
-import "./App.css";
-import Layout from "./components/layout/layout";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
-      <Layout >
-        <h1>Ecommerce App</h1>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/pageNotFound" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
