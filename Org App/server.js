b6ccddc3d1c9e56580da8813cf05a4949bbc8ae8
farhanwaseer <farhanaliwaseer@gmail.com>
2025-05-30 21:22:29 +0500

    auth routes

--- Org App/server.js
+++ Org App/server.js
@@ -3,6 +3,7 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
 // config dotenv
 dotenv.config();
@@ -17,10 +18,12 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
+// routes 
+app.use("/api/v1/auth", authRoutes)
 
 // rest api
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("<h1>Hello World -- ORG--APP</h1>");
 });
 
 const PORT = process.env.PORT;
@@ -28,6 +31,7 @@ const PORT = process.env.PORT;
 // run listen
 app.listen(PORT, () => {
   console.log(
-    `Server is running ${process.env.DEV_MODE} mode on port ${PORT}`.green.bold
+    `  Server is running ${process.env.DEV_MODE} mode on port ${PORT}`.green
+      .bold
   );
 });
