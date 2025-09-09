fd01f522d2be28bda0401ea83a40bdafb8967b72
farhanwaseer <farhanaliwaseer@gmail.com>
2025-09-10 03:03:53 +0500

    /

--- Car-on-Rent/client/src/pages/Home.jsx
+++ Car-on-Rent/client/src/pages/Home.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import CarCards from '../components/CarCards'
+import React from "react";
+import Hero from "../components/Hero";
+import FeaturedSection from "../components/FeaturedSection";
 
 const Home = () => {
   return (
     <>
-    <Hero />
-    {/* <CarCards /> */}
+      <Hero />
+      <FeaturedSection />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
