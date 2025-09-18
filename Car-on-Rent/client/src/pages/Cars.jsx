8e8fdc4a5922a2890cc5889c49542c050f508748
farhanwaseer <farhanaliwaseer@gmail.com>
2025-09-19 00:27:07 +0500

    car list compelet

--- Car-on-Rent/client/src/pages/Cars.jsx
+++ Car-on-Rent/client/src/pages/Cars.jsx
@@ -1,19 +1,47 @@
-import { assets } from "../assets/assets";
+import { useState } from "react";
+import { assets, dummyCarData } from "../assets/assets";
 import Title from "./../components/Title";
+import CarDetails from "./CarDetails";
+import CarCard from './../components/CarCard';
 const Cars = () => {
+  const [input, setInput] = useState();
   return (
     <div>
       <div className="flex flex-col items-center py-20 bg-light max-md:py-4">
-        <Title  title='Avaliable Cars' subTitle='Browse our selection of premium vehicles avaliable for your next adventure  '/>
-      
-      <div className="flex items-center  bg-white px-4 mt-6 max-w-140 w-full h-12 rounded-full shadow">
-<img src={assets.search_icon} alt="" className="w-4.5 h-4.5 mr-2" />
-<input type="text" placeholder="Search by make, model or feature " className="w-full h-full outline-none text-gray-500" />
-<img src={assets.filter_icon} alt="" className="w-4.5 h-4.5 mr-2 ml-2 " />
-      </div>
+        <Title
+          title="Avaliable Cars"
+          subTitle="Browse our selection of premium vehicles avaliable for your next adventure  "
+        />
+
+        <div className="flex items-center  bg-white px-4 mt-6 max-w-140 w-full h-12 rounded-full shadow">
+          <img src={assets.search_icon} alt="" className="w-4.5 h-4.5 mr-2" />
+          <input
+            onClick={(e) => setInput(e.target.value)}
+            value={input}
+            type="text"
+            placeholder="Search by make, model or feature "
+            className="w-full h-full outline-none text-gray-500"
+          />
+          <img
+            src={assets.filter_icon}
+            alt=""
+            className="w-4.5 h-4.5 mr-2 ml-2 "
+          />
+        </div>
       </div>
 
-      <div></div>
+      <div className="px-6 md:px-16  lg:px-24 xl:px-32  mt-10">
+        <p>Showing {dummyCarData.length } Cars </p>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4 xl:px-20 max-w-7xl mx-auto ">
+          {dummyCarData.map((car, index) =>(
+            <div key={index}>
+              <CarCard  car={car}/>
+
+            </div>
+          ) ) }
+        </div>
+      </div>
     </div>
   );
 };
