01cdd75d7bb31541d04a5a3f0130759d25ef5ecf
farhanwaseer <farhanaliwaseer@gmail.com>
2025-09-20 23:04:41 +0500

    navbar owner

---
+++ Car-on-Rent/client/src/components/owner/NavbarOwner.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { assets, dummyUserData } from "./../../assets/assets";
+
+const NavbarOwner = () => {
+  const user = dummyUserData;
+  return (
+    <div
+      className="flex items-center justify-between px-6 
+    md:px-10 py-4 text-gray-500 border-b border-borderColor relative transition-all "
+    >
+        <link to="/">
+        <img src={assets.logo} alt="" className="h-7" />
+        </link>
+        <p>Welcome, {user.name || "Owner"}</p>
+    </div>
+  );
+};
+
+export default NavbarOwner;
