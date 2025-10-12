472207d14136aab4afaad61a56d19e5e4e558738
farhanwaseer <farhanaliwaseer@gmail.com>
2025-10-12 18:51:08 +0500

    list car api , delete car api , car toggle api , created

--- Car-on-Rent/server/controllers/ownerController.js
+++ Car-on-Rent/server/controllers/ownerController.js
@@ -3,7 +3,8 @@ import User from "../models/User.js";
 import fs from "fs";
 import imagekit from "../configs/imageKit.js";
 import Car from "./../models/Car.js";
-import upload from './../middleware/multer.js';
+import upload from "./../middleware/multer.js";
+import { error } from "console";
 
 //  Change Role
 export const changeRoleToOwner = async (req, res) => {
@@ -49,3 +50,72 @@ export const addCar = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// Api to list all owner cars
+export const getOwnerCars = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const cars = await Car.find({ owner: _id });
+    res.json({ success: true, cars });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to toggle Car Availability
+
+export const toggleCarAvailability = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { carId } = req.body;
+    const car = await Car.find({ owner: _id });
+    // Checking  is car belongs to the user
+    if (car.owner.toString() !== _id.toString()) {
+      return res.json({ success: false, message: "Unauthorized" });
+    }
+    car.isAvaliable = !car.isAvaliable;
+    await car.save();
+    res.json({ success: true, message: "Availability Toggled" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to Delete Car
+
+export const deletCar = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { carId } = req.body;
+    const car = await Car.find({ owner: _id });
+    // Checking  is car belongs to the user
+    if (car.owner.toString() !== _id.toString()) {
+      return res.json({ success: false, message: "Unauthorized" });
+    }
+    car.owner = null;
+    car.isAvaliable = false;
+    await car.save();
+    res.json({ success: true, message: "Car Removed" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//  API to get Dashboard Data
+export const getDashboardData = async (req, res) => {
+  try {
+    const { _id, role } = req.user;
+    if (role !== "owner") {
+      return res.json({ success: false, message: "Unauthorized" });
+    }
+    const cars = await Car.find({ owner: _id });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// =bn
